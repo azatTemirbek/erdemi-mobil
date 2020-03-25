c684c215a18821e846ad94271d6a3c9269621eb3
Azat Temirbek uulu <azat.4alabs@erdemir.com.tr>
2020-03-25 18:01:31 +0300

    lol

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import React from "react";
 
 function Al(props){
     return(
-        <View><Text>15Version+{props.text}</Text></View>
+        <View><Text>16Version+{props.text}</Text></View>
     )
 }
 export {
