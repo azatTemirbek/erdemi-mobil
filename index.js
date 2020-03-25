dce552f9d330a138e3b319f8d207a7cdad56c6fe
Azat Temirbek uulu <azat.4alabs@erdemir.com.tr>
2020-03-25 14:59:59 +0300

    initial

---
+++ index.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+function Al(props){
+    return(
+        <View><Text>yeniVerrsion+{props.text}</Text></View>
+    )
+}
+export {
+    Al
+};
+
+export default Al
