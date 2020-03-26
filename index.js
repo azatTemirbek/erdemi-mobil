d4255f2ee60b008cbb56230673dd3d56f2dc419b
Azat Temirbek uulu <azat.4alabs@erdemir.com.tr>
2020-03-26 11:53:15 +0300

    Table Component Merged

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-import { View, Text } from "react-native";
-import React from "react";
-
-function Al(props){
-    return(
-        <View><Text>17Version+{props.text}</Text></View>
-    )
-}
-export {
-    Al
-};
-
-export default Al
+import Table from "./components/Table"
+export { Table };
+export default {};
