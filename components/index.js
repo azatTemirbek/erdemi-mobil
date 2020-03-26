f1c25384e3f4539719de80d390f8f0f98d0e2b2f
Azat Temirbek uulu <azat.4alabs@erdemir.com.tr>
2020-03-26 15:45:19 +0300

    commitprepush

--- components/index.js
+++ components/index.js
@@ -1,11 +1,11 @@
 import Table from "./Table";
 import Text from "./Text";
 import Image from "./Image";
-import Card from "./Card"
-import Icon from "./Icon"
-import Button from "./Button"
-import Block from "./Block"
-import MapArray from "./MapArray"
+import Card from "./Card";
+import Icon from "./Icon";
+import Button from "./Button";
+import Block from "./Block";
+import MapArray from "./MapArray";
 
 export { Table, Text, Image, Card, Icon, Button, Block, MapArray };
-export default {}; 
+export default {};
