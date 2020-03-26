889e653b052f8833bbd8b4f679d8348f60cafad2
Azat Temirbek uulu <azat.4alabs@erdemir.com.tr>
2020-03-26 14:15:56 +0300

    card fix

--- components/Card/index.js
+++ components/Card/index.js
@@ -2,19 +2,16 @@ import React, { Component } from "react";
 import { View, TouchableOpacity } from "react-native";
 import styles from "./styles";
 import PropTypes from "prop-types";
-import Image  from "../";
+import Image from "../";
 
-export default class Card extends Component {
-  render() {
-    const { style, children, styleContent, image, onPress } = this.props;
-    return (
-      <TouchableOpacity style={style} onPress={onPress} activeOpacity={0.9}>
-        <Image source={image} style={styles.imageBanner} />
-        <View style={styleContent}>{children}</View>
-      </TouchableOpacity>
-    );
-  }
-}
+const Card = ({ style, children, styleContent, image, onPress }) => {
+  return (
+    <TouchableOpacity style={style} onPress={onPress} activeOpacity={0.9}>
+      {!!image && <Image source={image} style={styles.imageBanner} />}
+      <View style={styleContent}>{children}</View>
+    </TouchableOpacity>
+  );
+};
 
 Card.propTypes = {
   image: PropTypes.any,
@@ -37,3 +34,5 @@ Card.defaultProps = {
   },
   onPress: () => {}
 };
+
+export default Card;
