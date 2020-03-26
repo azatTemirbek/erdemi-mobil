0042f862ca4313d0a05bdb61449fb92c529835f6
Azat Temirbek uulu <azat.4alabs@erdemir.com.tr>
2020-03-26 14:11:43 +0300

    commitprepush

--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
-export * from "./Table";
-export * from "./Text";
-export * from "./Image";
-export * from "./Card"
+import Table from "./Table";
+import Text from "./Text";
+import Image from "./Image";
+import Card from "./Card"
+export { Table, Text, Image, Card };
 export default {};
