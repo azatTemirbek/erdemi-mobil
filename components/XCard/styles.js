7fee4c58e19350907ed8d9a57d872766943480cb
Azat Temirbek uulu <azat.4alabs@erdemir.com.tr>
2020-04-01 16:47:59 +0300

    commitpremerge

--- components/XCard/styles.js
+++ components/XCard/styles.js
@@ -3,7 +3,7 @@ import { generateColors } from "../../config";
 import { elevationShadowStyle } from "../../utils";
 export default StyleSheet.create({
   container: {
-    ...elevationShadowStyle(4),
+    ...elevationShadowStyle(2),
     borderRadius: 10,
     borderWidth: 0.02,
     borderColor: "transparent",
