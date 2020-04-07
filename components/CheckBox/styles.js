1b343eb564b530aa5d9b5323fb8c1b73a5ad5c58
Azat Temirbek uulu <azat.4alabs@erdemir.com.tr>
2020-04-07 11:48:29 +0300

    commitpremerge

--- components/CheckBox/styles.js
+++ components/CheckBox/styles.js
@@ -3,11 +3,8 @@ import { BaseColor } from "../../config";
 
 export default StyleSheet.create({
   container: {
-    padding: 5,
     borderRadius: 3,
     borderColor: BaseColor.dividerColor,
-    borderWidth: 0.5,
-    alignItems: "center",
-    justifyContent: "center"
+    borderWidth: 0.5
   }
 });
