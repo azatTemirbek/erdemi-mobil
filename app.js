98e435200ac34a3472ea4943eb82c8bfdd0a96e1
Azat Temirbek uulu <azat.4alabs@erdemir.com.tr>
2020-04-09 14:46:25 +0300

    commitpremerge

---
+++ app.js
@@ -0,0 +1,4 @@
+import {AppRegistry} from "react-native";
+import StorybookUIRoot from "./storybook/index";
+import {name as appName} from "./app.json";
+AppRegistry.registerComponent(appName, () => StorybookUIRoot);
