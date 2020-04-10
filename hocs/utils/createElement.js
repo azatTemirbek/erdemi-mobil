700c73865f18b20e7b9e34e68f2c538e36f2f9c9
Azat Temirbek uulu <azat.4alabs@erdemir.com.tr>
2020-04-10 13:04:13 +0300

    commitpremerge

--- hocs/utils/createElement.js
+++ hocs/utils/createElement.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, {createElement as CE, memo} from "react";
+import isClassComponent from "./isClassComponent";
 /**
  * used to merge the dom view
  * @param {Symbol} Component
  */
 export const createElement = (Component, {wrappedComponentRef, ...props}) => {
   props.ref = wrappedComponentRef ? wrappedComponentRef : props.ref;
-  return typeof Component === "function"
+  try {
+    return (!isClassComponent(Component) && typeof Component === "function")
     ? Component(props)
     : React.createElement(Component, props);
+  } catch (error) {
+    console.log(error)
+    return CE(Component, props)
+  }finally {
+    return CE(Component, props)
+  }
 };
 
 export default createElement;
