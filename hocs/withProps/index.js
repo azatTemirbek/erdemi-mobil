ca4e7d384eaca550570bf8abfac75083fd3292a1
Azat Temirbek uulu <azat.4alabs@erdemir.com.tr>
2020-05-06 12:26:28 +0300

    hoc try with ramda

--- hocs/withProps/index.js
+++ hocs/withProps/index.js
@@ -1,18 +1,23 @@
 import hoistStatics from "hoist-non-react-statics";
 import createElement from "../utils/createElement";
-import {curry} from "ramda";
+import {curry, merge} from "ramda";
 /**
  * @param {Object|Function} extraProps used to add extra props to compose
  */
-export const withProps = curry((extraProps = {}, Component) => {
+// withProps:: Object -> Component -> Component
+export const withProps = curry((extraProps, Component) => {
+  extraProps = extraProps || {};
   const displayName = `withProps(${Component.displayName || Component.name})`;
   const C = (remainingProps) =>
-    createElement(Component, {
-      ...remainingProps,
-      ...(typeof extraProps === "function"
-        ? extraProps(remainingProps)
-        : extraProps)
-    });
+    createElement(
+      Component,
+      merge(
+        remainingProps,
+        typeof extraProps === "function"
+          ? extraProps(remainingProps)
+          : extraProps
+      )
+    );
   C.displayName = displayName;
   C.WrappedComponent = Component;
   /** used to copy static methods */
